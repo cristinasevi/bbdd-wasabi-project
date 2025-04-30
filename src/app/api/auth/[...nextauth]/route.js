156b0ca0980662533a63c9296128824e06e57914
cristinasevi <cristina.csevi@gmail.com>
2025-04-30 17:45:12 +0200

    Update Login Access with Account

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { pool  } from "../../lib/db.js"
 
 export const authOptions = {
   providers: [
@@ -9,13 +10,43 @@ export const authOptions = {
     }),
   ],
   pages: {
-    signIn: "/login",
+    signIn: "/",
   },
   callbacks: {
+    async signIn({ user, account }) {
+      if (account.provider === "google") {
+        try {
+          const [rows] = await pool.execute(
+            "SELECT * FROM Usuario WHERE Email = ? LIMIT 1",
+            [user.email]
+          )
+
+          console.log("Email recibido de Google:", user.email)
+          console.log("Resultado de la query:", rows)
+
+
+          if (rows.length === 0) {
+            console.log("Acceso denegado para:", user.email)
+            return false
+          }
+
+          console.log("Acceso permitido para:", user.email)
+          return true
+        } catch (err) {
+          console.error("Error consultando la base de datos:", err)
+          return false
+        }
+      }
+
+      return true
+    },
+
     async session({ session, token }) {
+      session.user.id = token.id
       return session
     },
-    async jwt({ token, user, account }) {
+
+    async jwt({ token, user }) {
       if (user) {
         token.id = user.id
       }
